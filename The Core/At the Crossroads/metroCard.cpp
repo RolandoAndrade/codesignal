f2ff2f2fbba751fe434b5e4950b457e4a0ef29dd
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-03-18 15:21:50 -0400

    metroCard

--- The Core/At the Crossroads/metroCard.cpp
+++ The Core/At the Crossroads/metroCard.cpp
@@ -1,8 +1,6 @@
 vector<int> metroCard(int d) 
 {
-    if(d==30)
-        return vector({31});
-    if(d==28)
-        return vector({30});
-    return vector({28,30,31});
+    if(d==30||d==28)
+        return {31};
+    return {28,30,31};
 }
